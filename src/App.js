c79de5093cfb7281bc4ee9d9754982650778ea83
parade <852791765@qq.com>
2022-11-16 23:58:22 +0800

    项目前置准备

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+// 导入路由
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+// 导入页面组件
+import Login from './pages/Login'
+import Layout from './pages/Layout'
 
 function App () {
   return (
-    <div>root</div>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Layout />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import { Button } from "antd"
+
+const Login = () => {
+  return (
+    <div>
+      <Button type="primary">Button</Button>
+    </div>
+  )
+}
+
+export default Login
