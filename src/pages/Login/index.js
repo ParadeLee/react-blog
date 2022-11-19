799df15beb8f23271787cfc52e1710043c7ad96a
parade <852791765@qq.com>
2022-11-19 16:58:06 +0800

    更新源码

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,53 +1,81 @@
-import { Card, Form, Input, Button, Checkbox } from "antd"
-// import logo from '@/assets'
+import { Card, Form, Input, Checkbox, Button, message } from 'antd'
+import logo from '@/assets/logo.png'
+import { useNavigate } from 'react-router-dom'
+// 导入样式文件
 import './index.scss'
-
-const Login = () => {
-  const onFinish = values => {
+import { useStore } from '@/store'
+function Login () {
+  const { loginStore } = useStore()
+  const navigate = useNavigate()
+  async function onFinish (values) {
     console.log(values)
+    // values：放置的是所有表单项中用户输入的内容
+    // todo:登录
+    const { mobile, code } = values
+    await loginStore.getToken({ mobile, code })
+    // 跳转首页
+    navigate('/', { replace: true })
+    // 提示用户
+    message.success('登录成功')
   }
+
   return (
     <div className="login">
       <Card className="login-container">
-        <img className="login-logo" src="" alt="" />
-        {/* 登陆表单 */}
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        {/* 子项用到的触发事件 需要在Form中都声明一下才可以 */}
         <Form
-          onFinish={onFinish}
+          validateTrigger={['onBlur', 'onChange']}
           initialValues={{
             remember: true,
+            mobile: '13811111111',
+            code: '246810'
           }}
-          validateTrigger={['onBlur', 'onChange']}
+          onFinish={onFinish}
         >
           <Form.Item
-            name='mobile'
+            name="mobile"
             rules={[
               {
-                pattern: /^1[3-9]\d{9}$/,
-                message: 'wrong mobile phone',
-                validateTrigger: 'onBlur'
+                required: true,
+                message: '请输入手机号',
               },
-              { required: true, message: '请输入手机号' }
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号',
+                validateTrigger: 'onBlur'
+              }
             ]}
           >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
           <Form.Item
-            name='code'
+            name="code"
             rules={[
-              { len: 6, message: 'code 6 number', validateTrigger: 'onBlur' },
-              { required: true, message: '请输入验证码' }
+              {
+                required: true,
+                message: '请输入密码',
+              },
+              {
+                len: 6,
+                message: '请输入6位密码',
+                validateTrigger: 'onBlur'
+              }
             ]}
           >
             <Input size="large" placeholder="请输入验证码" />
           </Form.Item>
           <Form.Item
-            name='remember'
+            name="remember"
             valuePropName="checked"
+
           >
             <Checkbox className="login-checkbox-label">
               我已阅读并同意「用户协议」和「隐私条款」
             </Checkbox>
           </Form.Item>
+
           <Form.Item>
             <Button type="primary" htmlType="submit" size="large" block>
               登录
--- src/store/login.Store.js
+++ src/store/login.Store.js
@@ -1,17 +1,27 @@
+// login module
 import { makeAutoObservable } from 'mobx'
-import { http } from '@/utils'
-
+import { http, setToken, getToken, removeToken } from '@/utils'
 class LoginStore {
-  token = ''
+  token = getToken() || ''
   constructor() {
-    makeAutoObservable()
+    // 响应式
+    makeAutoObservable(this)
   }
-  // 登录
-  login = async ({ mobile, code }) => {
-    const res = await http.post('', {
+  getToken = async ({ mobile, code }) => {
+    // 调用登录接口
+    const res = await http.post('http://geek.itheima.net/v1_0/authorizations', {
       mobile, code
     })
+    // 存入token
     this.token = res.data.token
+    // 存入ls
+    setToken(this.token)
+  }
+  // 退出登录
+  loginOut = () => {
+    this.token = ''
+    removeToken()
   }
 }
+
 export default LoginStore
